f3436e35a2be55b0272a53391eb7aabd186bde65
mingtingjie <exphet@163.com>
2019-03-18 15:52:58 +0800

    test git

--- app/src/main/java/com/android/pc/map/Constants.java
+++ app/src/main/java/com/android/pc/map/Constants.java
@@ -1,9 +1,24 @@
 package com.android.pc.map;
 
+import com.amap.api.maps.model.LatLng;
+
 public class Constants {
 
     public static final String Key_GaodeMap = "013405c13d248541e2fc3282b332ecdc";
 
+    public static final LatLng BEIJING = new LatLng(39.90403, 116.407525);// 北京市经纬度
+    public static final LatLng ZHONGGUANCUN = new LatLng(39.983456, 116.3154950);// 北京市中关村经纬度
+    public static final LatLng SHANGHAI = new LatLng(31.238068, 121.501654);// 上海市经纬度
+    public static final LatLng FANGHENG = new LatLng(39.989614, 116.481763);// 方恒国际中心经纬度
+    public static final LatLng CHENGDU = new LatLng(30.679879, 104.064855);// 成都市经纬度
+    public static final LatLng XIAN = new LatLng(34.341568, 108.940174);// 西安市经纬度
+    public static final LatLng ZHENGZHOU = new LatLng(34.7466, 113.625367);// 郑州市经纬度
+    public static final LatLng DONGHU = new LatLng(30.553943071825763,114.41474621397023);//武昌东湖  114.41474621397023,30.553943071825763
+    public static final LatLng GONGMAOXUEYUAN  = new LatLng(30.52881354565407,114.41524885521083);//武汉工贸职业技术学院  114.41524885521083,30.52881354565407
+    public static final LatLng WUDANG  = new LatLng(30.54229808700317,114.41286062071916);//114.41286062071916,30.54229808700317//武当会馆
 
+
+    public static final LatLng LANHUADAO = new LatLng(30.54611213854209,114.42440943838693);//114.42440943838693,30.54611213854209
+    public static final LatLng NONGJIACAI = new LatLng(30.538222789396833,114.41930879359617);//114.41930879359617,30.538222789396833
 
 }
