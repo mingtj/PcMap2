7bd6e6ebf5864f27ba8e36c0494c4b9527f3fc00
mingtingjie <exphet@163.com>
2019-03-18 15:52:48 +0800

    test git

--- app/src/main/java/com/android/pc/map/TestMapActivity.java
+++ app/src/main/java/com/android/pc/map/TestMapActivity.java
@@ -2,22 +2,62 @@ package com.android.pc.map;
 
 import android.location.Location;
 import android.location.LocationListener;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdate;
+import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.CameraPosition;
 import com.amap.api.maps.model.Circle;
 import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.Marker;
 import com.amap.api.maps.model.MarkerOptions;
 import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.maps.model.NavigateArrowOptions;
 import com.amap.api.maps.model.Polygon;
 import com.amap.api.maps.model.Polyline;
+import com.amap.api.maps.model.PolylineOptions;
 
-public class TestMapActivity extends AppCompatActivity implements LocationListener {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletionService;
 
+public class TestMapActivity extends AppCompatActivity {
+
+
+
+
+    private static final int  CHANGE_CENTER = 1000;
+    private static final int  NvaLine1 = 1001;
+    private static final int  NvaLine2 = 1002;
+    private LatLng LastDot;
+
+
+     Handler mHandler = new Handler(){
+         public void handleMessage(Message msg) {
+             switch (msg.what) {
+                 case CHANGE_CENTER:
+                     changePositionCenter(ll);
+                     break;
+                 case NvaLine1:
+                     NvaLine();
+                     DrawLine(Constants.DONGHU,Constants.LANHUADAO);
+                     break;
+                 case NvaLine2:
+                     NvaLine2();
+                     list.add(Constants.NONGJIACAI);
+                     DrawLine(LastDot,Constants.NONGJIACAI);
+                     break;
+             }
+             super.handleMessage(msg);
+         }
+     };
 
     private MapView mMapView = null;
 
@@ -54,8 +94,13 @@ public class TestMapActivity extends AppCompatActivity implements LocationListen
             aMap = mMapView.getMap();
         }
 
+        //markerDot(ll);
 
-        markerDot(ll);
+         list = new ArrayList<>();
+        list.add(Constants.DONGHU);
+        list.add(Constants.LANHUADAO);
+
+        mHandler.sendEmptyMessageDelayed(NvaLine1,2000);
     }
 
 
@@ -63,10 +108,6 @@ public class TestMapActivity extends AppCompatActivity implements LocationListen
         super.onDestroy();
         //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
         mMapView.onDestroy();
-
-//        if(null != mlocationClient){
-//            mlocationClient.onDestroy();
-//        }
     }
     @Override
     protected void onResume() {
@@ -89,55 +130,113 @@ public class TestMapActivity extends AppCompatActivity implements LocationListen
 
 
 
-    //实现定位蓝点
-    private void dingWeiDot(){
+    private void NvaLine(){
+            // 加一个箭头对象（NavigateArrow）对象在地图上
+            aMap.addNavigateArrow(new NavigateArrowOptions().add(Constants.DONGHU,
+                    Constants.WUDANG).width(20));
+            aMap.moveCamera(CameraUpdateFactory
+                    .newCameraPosition(new CameraPosition(Constants.WUDANG, 16f, 38.5f, 300)));
 
-        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
-        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
-        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE) ;//定位一次，且将视角移动到地图中心点。
+            markerDot(Constants.WUDANG,"武昌会馆","湖北省武汉市武昌区鲁磨路235号");
 
-
-//        aMap.setLocationSource(this);
-        //aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
-        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
-        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-//        if (mListener != null&&amapLocation != null) {
-//            if (amapLocation != null
-//                    &&amapLocation.getErrorCode() == 0) {
-//                mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
-//            } else {
-//                String errText = "定位失败," + amapLocation.getErrorCode()+ ": " + amapLocation.getErrorInfo();
-//                Log.e("AmapErr",errText);
-//            }
-//        }
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
+            mHandler.sendEmptyMessageDelayed(NvaLine2,5000);
 
     }
 
 
-    private void markerDot(LatLng latLng){
+    List<LatLng> list;
+    private void DrawLine(LatLng startDot,LatLng endDot){
+        //划线
+        aMap.addPolyline(new PolylineOptions()
+                .addAll(list)
+                .width(20f));
+
+        //转换中心点到endDot
+        aMap.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(endDot,16f,38.5f,300)));//CameraPosition(LatLng target, float zoom, float tilt, float bearing)  tilt:目标可视区域的倾斜度，以角度为单位;bearing:可视区域指向的方向，以角度为单位，从正北向逆时针方向计算，从0 度到360 度。
+
+
+        //add marker
+        for (int i =1;i<list.size();i++){
+            markerDot(list.get(i),"位置"+i,"内容"+i);
+        }
+
+
+        LastDot = endDot;
+
+        /**
+         *
+
+         LatLng A = new LatLng(Lat_A, Lon_A);
+         LatLng B = new LatLng(Lat_B, Lon_B);
+         LatLng C = new LatLng(Lat_C, Lon_C);
+         LatLng D = new LatLng(Lat_D, Lon_D);
+         aMap.addPolyline((new PolylineOptions())
+         .add(A, B, C, D)
+         .width(10)
+         .color(Color.argb(255, 1, 255, 255)));
+
+         */
+    }
+
+
+
+
+
+    private void NvaLine2(){
+        aMap.clear();
+
+        // 加一个箭头对象（NavigateArrow）对象在地图上
+        aMap.addNavigateArrow(new NavigateArrowOptions().add(Constants.DONGHU,Constants.WUDANG,
+                Constants.GONGMAOXUEYUAN).width(20));
+        aMap.moveCamera(CameraUpdateFactory
+                .newCameraPosition(new CameraPosition(Constants.GONGMAOXUEYUAN, 16f, 38.5f, 300)));
+
+        markerDot(Constants.WUDANG,"武昌会馆","湖北省武汉市武昌区鲁磨路235号");
+
+        markerDot(Constants.GONGMAOXUEYUAN,"武汉工贸学院","湖北省武汉市武昌区大学城316号");
+
+    }
+
+    //标记某个点位，并转换中心点到这个位置
+    private void markerDot(LatLng latLng,String title,String centent){
         //湖北省武汉市武昌区沿湖大道16号
 //        LatLng latLng = new LatLng(39.906901,116.397972);
-        final Marker marker = aMap.addMarker(new MarkerOptions().position(latLng).title("湖北省武汉市武昌区沿湖大道16号").snippet("DefaultMarker"));
-        marker.isVisible();
+        aMap.addMarker(new MarkerOptions().position(latLng).title(title).snippet(centent));
+    }
+
+//美好长江  美好名流汇  首地云梦台  东原乐见城  地铁盛观上城
+    private void changePositionCenter(LatLng latLng){
+        //转换中心点，并设置放大倍数
+//        changeCamera(
+//                CameraUpdateFactory.newCameraPosition(new CameraPosition(
+//                        Constants.DONGHU, 18, 30, 0)));
+
+
+//       //指定latlng并设置中心点
+//        CameraUpdate cu = CameraUpdateFactory.newLatLng(latLng);
+//        changeCamera(cu);
+
+        //设置地图中心点以及缩放级别。   latLng - 地图中心点。  zoom - 缩放级别，[3-20]，数字越大，显示越细致
+        CameraUpdate cu2 = CameraUpdateFactory.newLatLngZoom(latLng,13);
+        changeCamera(cu2);
+
+
+        //添加marker点
+        aMap.clear();
+        aMap.addMarker(new MarkerOptions().position(Constants.DONGHU)
+                        .title("武汉东湖风景区")//title
+                        .snippet("湖北省武汉市武昌区沿湖大道16号")  //content
+                // .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+        );
+    }
+
+    /**
+     * 根据动画按钮状态，调用函数animateCamera或moveCamera来改变可视区域
+     */
+    private void changeCamera(CameraUpdate update) {
+
+        aMap.moveCamera(update);
+
     }
 
 
